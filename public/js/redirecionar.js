c40a9541f4ffcc968d3cca18a34870e25dd450ce
Kauan Henrique <82743365+PlayerX-000@users.noreply.github.com>
2021-12-03 14:01:17 -0300

    quinto

---
+++ public/js/redirecionar.js
@@ -0,0 +1,11 @@
+$('#icongit').click(function() { 
+    document.location = 'https://github.com/PlayerX-000/PetTour/';
+   } );
+
+   $('#iconface').click(function() { 
+    document.location = 'https://facebook.com.br/';
+   } );
+
+   $('#iconinsta').click(function() { 
+    document.location = 'https://instagram.com.br/';
+   } );
