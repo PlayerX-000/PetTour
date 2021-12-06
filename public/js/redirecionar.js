636db85fc87c4c64442dc09a2c3a3c3dc749029e
PlayerX-000 <kauan.mghenrique@gmail.com>
2021-12-05 22:39:31 -0300

    update

--- public/js/redirecionar.js
+++ public/js/redirecionar.js
@@ -1,12 +1,12 @@
 $('#icongit').click(function() { 
-    document.open('https://github.com/PlayerX-000/PetTour/');
+    window.open('https://github.com/PlayerX-000/PetTour/');
 
    } );
 
    $('#iconface').click(function() { 
-    document.open('https://facebook.com/');
+    window.open('https://facebook.com/');
    } );
 
    $('#iconinsta').click(function() { 
-    document.open('https://instagram.com/');
+    window.open('https://instagram.com/');
    } );
